3e19c7e889b2a6937d7f5542538775872f477247
Cody Lepp <cdylpp@yahoo.com>
2021-04-25 19:57:32 -0700

    Added circumference

--- circle.cpp
+++ circle.cpp
@@ -3,14 +3,23 @@ using namespace std;
 
 //This program prompts user to enter circle raduis
 //Then the circle area is displayed
+//C = 2*PI*r
+const double PI = 3.14159;
+double getCircumference(double radius);
+double getArea(double radius);
 
 int main()
 {
-	const double PI = 3.14159;
 	double radius;
 	cout << "Enter radius\n";
 	cin >> radius;
-	cout << "The area of a circle with radius \n";
-	cout << PI * radius * radius;
+	cout << "The area of the circle is " << getArea(radius) << " and its circumference is " << getCircumference(radius) << endl;
 	return 0;
+}
+
+double getCircumference(double radius) {
+	return (2 * PI * radius);
+}
+double getArea(double radius) {
+	return (PI * radius * radius);
 }
